a40cca1682d1ffd1520da7bfc21f8a71f715092f
Philipp Kessling <p.kessling@leibniz-hbi.de>
2023-03-22 07:57:44 +0100

    fix: missing __version__ in __init__.py

--- {{ cookiecutter.project_slug }}/tests/test_package.py
+++ {{ cookiecutter.project_slug }}/tests/test_package.py
@@ -1,11 +1,12 @@
 """Test suite for package-level items."""
+import re
 
 from {{ cookiecutter.project_slug }} import __version__
 
 
 def test_version():
     """Should be the current version number."""
-    assert __version__ == "0.3.3"
+    assert __version__ == "1.0.0"
 
 
 def test_version_type():
@@ -15,4 +16,4 @@ def test_version_type():
 
 def test_version_number_is_semver():
     """Should be a semver."""
-    assert all([int(x) for x in __version__.split(".")])
+    assert re.match(r"\d+\.\d+\.\d+-{0,1}\w*", __version__) is not None
---
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__init__.py
@@ -0,0 +1,3 @@
+"""{{ cookiecutter.project_slug }}'s main module."""
+
+__version__ = "1.0.0"
