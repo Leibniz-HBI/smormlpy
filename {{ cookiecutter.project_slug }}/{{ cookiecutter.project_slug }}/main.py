c05a0b1a612572d3d0a915d067bb8a322601f5c0
Philipp Kessling <p.kessling@leibniz-hbi.de>
2023-03-22 10:45:04 +0100

    fix: better documentation and consistency

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/main.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/main.py
@@ -1,22 +1,27 @@
 import click
-from loguru import logger as log
 
 @click.command()
 @click.argument("name", type=str)
 def cli(name: str) -> None:
-    """ This internal function is called by the click-decorated function.
-    The split into two functions is necessary for documentation purposes as pdoc3
-    cannot process click-decorated functions.
+    """Entry point for our CLI.
+
+    In here we delegate incoming commands from the terminal to our program;
+    for this example, we receive a string, which we want to interpolate.
+    Decoupling the user interface code (which goes in to this function) from
+    the implementation of the internal logic is important.
+
+    E.g. testing the `greet`-function on it's own is trivial, while testing the
+    same functionality if we would program the functionality into the `cli`-function
+    would be quite difficult. Same is true, if we would want to change and extend
+    the behavior of the implementation.
 
     Parameters :
         name : str : name argument passed by click
-    Returns:
-        None: Nada   
     """
-    log.info(cli_implementation(name))
+    click.echo(greet(name))
 
-def cli_implementation(name: str) -> None:
-    """ Greet someone or something by name.
+def greet(name: str) -> str:
+    """Greet someone or something by name.
 
     Parameters:
         name : str : Whom to greet
@@ -25,6 +30,3 @@ def cli_implementation(name: str) -> None:
         str : Greeting
     """
     return f"Hello {name}!"
-
-if __name__ == "main":
-    cli()
